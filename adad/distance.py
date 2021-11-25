93f570db23107a42f5bbb6b58e709cb18521e878
bonjuruu <sarah.kyounghui@gmail.com>
2021-11-25 22:33:06 +1300

    Added DA-index kappa and delta

--- adad/distance.py
+++ adad/distance.py
@@ -71,3 +71,97 @@ class DAIndexGamma(AppDomainBase):
         # TODO: We might need consider to return `measure`(float) instead of 
         # binary values.
         return results
+    
+class DAIndexKappa(AppDomainBase):
+    """Distance based Applicability Domain (DA-Index Kappa)
+
+    Parameters
+    ----------
+    clf: classifier, optional
+        A pre-trained classifier. It should have `predict` and `predict_proba`
+        methods.
+    k: int, default=5
+        The number of K-nearest neighbors.
+    ci: float, default=0.95
+        Confidence interval. It should in-between (0, 1].
+    """
+
+    def __init__(self, clf=None, k=5, ci=0.95, dist_metric='euclidean'):
+        super(DAIndexKappa, self).__init__()
+
+        self.clf = clf
+        self.k = k
+        self.ci = ci
+        self.dist_metric = dist_metric
+        assert dist_metric in DISTANCE_METRICS
+
+        self.tree = None
+        self.threshold = np.inf
+
+    def fit(self, X, y=None):
+        n = len(X)
+        self.tree = BallTree(X, metric=self.dist_metric)
+        dist, _ = self.tree.query(X, k=self.k + 1)
+        dist = dist[:, -1]
+        dist_sorted = np.sort(dist)
+        idx = int(np.floor(self.ci * n))
+        self.threshold = dist_sorted[idx]
+        return self
+
+    def measure(self, X):
+        """Check AD on X. Returns True if a sample is within the domain."""
+        dist, _ = self.tree.query(X, k=self.k + 1)
+        dist = dist[:, -1]
+        measure = dist/ self.threshold
+        # Less than 1 indicates the sample within the domain.
+        results = measure <= 1
+        # TODO: We might need consider to return `measure`(float) instead of 
+        # binary values.
+        return results
+    
+class DAIndexDelta(AppDomainBase):
+    """Length of mean vector based Applicability Domain (DA-Index Delta)
+
+    Parameters
+    ----------
+    clf: classifier, optional
+        A pre-trained classifier. It should have `predict` and `predict_proba`
+        methods.
+    k: int, default=5
+        The number of K-nearest neighbors.
+    ci: float, default=0.95
+        Confidence interval. It should in-between (0, 1].
+    """
+
+    def __init__(self, clf=None, k=5, ci=0.95, dist_metric='euclidean'):
+        super(DAIndexDelta, self).__init__()
+
+        self.clf = clf
+        self.k = k
+        self.ci = ci
+        self.dist_metric = dist_metric
+        assert dist_metric in DISTANCE_METRICS
+
+        self.tree = None
+        self.threshold = np.inf
+
+    def fit(self, X, y=None):
+        n = len(X)
+        self.tree = BallTree(X, metric=self.dist_metric)
+        dist, idx = self.tree.query(X, k=self.k + 1)
+        dist = [np.linalg.norm(np.sum(self.X[idx[i]], axis=0), 2) for i in range(len(idx))]
+        dist_sorted = np.sort(dist)
+        idx = int(np.floor(self.ci * n))
+        self.threshold = dist_sorted[idx]
+        return self
+
+    def measure(self, X):
+        """Check AD on X. Returns True if a sample is within the domain."""
+        dist, idx = self.tree.query(X, k=self.k + 1)
+        dist = [np.linalg.norm(np.sum(self.X[idx[i]], axis=0), 2) for i in range(len(idx))]
+        measure = dist/ self.threshold
+        # Less than 1 indicates the sample within the domain.
+        results = measure <= 1
+        # TODO: We might need consider to return `measure`(float) instead of 
+        # binary values.
+        return results
