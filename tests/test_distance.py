b9f48fa8ff88a2cfcdf2ef2b0d6e0a941fba89c0
Luke Chang <luke.x.chang@gmail.com>
2021-12-04 00:18:32 +1300

    [test] format code

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -1,9 +1,9 @@
 import os
-import pandas as pd
+
 import numpy as np
+import pandas as pd
 import pytest
 from numpy.random import randint
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
@@ -13,12 +13,14 @@ SEED = 348
 np.random.seed(348)
 
 X = randint(5, size=(20, 5))
-y = np.array([0,0,1,1,0,1,0,1,0,1,0,0,1,1,0,1,0,1,0,1])
+y = np.array([0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=SEED)
 
 classifier = RandomForestClassifier(random_state=SEED)
 classifier.fit(X_train, y_train)
+
 
 def test_Gamma_constructor():
     ad = DAIndexGamma(dist_metric='euclidean')
@@ -26,72 +28,78 @@ def test_Gamma_constructor():
 
     with pytest.raises(AssertionError) as e_info:
         ad = DAIndexGamma(dist_metric='asdkjnas')
-        
+
+
 def test_Gamma_fit():
     ad = DAIndexGamma()
     ad.fit(X_train)
-    
+
     dist, _ = ad.tree.query(X_train, k=ad.k + 1)
     dist_mean = np.sum(dist, axis=1) / ad.k
     dist_sorted = np.sort(dist_mean)
     idx = int(np.floor(ad.ci * len(X_train)))
-    
-    assert ad.threshold <= dist_sorted[len(X_train)- 1] and ad.threshold >= dist_sorted[idx - 1]
-    
+
+    assert ad.threshold <= dist_sorted[len(
+        X_train) - 1] and ad.threshold >= dist_sorted[idx - 1]
+
+
 def test_Gamma_measure():
     ad = DAIndexGamma(clf=classifier)
     ad.fit(X_train)
     results = ad.measure(X_train)
-    
+
     assert all([type(x) == np.bool_ for x in results])
-    
+
     results2 = ad.measure(X_test)
-    
+
     assert all([type(x) == np.bool_ for x in results2])
 
-    
+
 def test_Gamma_predict():
     ad = DAIndexGamma(clf=classifier)
     ad.fit(X_train)
     predict, indexs = ad.predict(X_test)
     p_size, i_size = len(predict), len(indexs)
-    
+
     assert i_size > 0 and i_size <= len(X_test)
     assert p_size > 0 and p_size <= len(X_test)
     assert i_size == p_size
-    
-    
+
+
 def test_Gamma_predict_proba():
     ad = DAIndexGamma(clf=classifier)
     ad.fit(X_train)
     predict, indexs = ad.predict_proba(X_test)
     p_size, i_size = len(predict), len(indexs)
-    
+
     assert i_size > 0 and i_size <= len(X_test)
     assert p_size > 0 and p_size <= len(X_test)
     assert i_size == p_size
-    
+
+
 def test_Gamma_score():
     ad = DAIndexGamma(clf=classifier)
     ad.fit(X_train)
     score = ad.score(X_test, y_test)
-    
+
     assert score >= 0 and score <= 1
-    
+
+
 def test_ad_save():
-    path = os.path.join(os.getcwd(), "tests//save_files") 
+    path = os.path.join(os.getcwd(), "tests//save_files")
     file_path = os.path.join(path, "test_save_ad")
     ad = DAIndexGamma(clf=classifier, dist_metric="seuclidean")
     ad.save(file_path)
-    
+
     assert os.path.exists(file_path)
-    
+
+
 def test_ad_load():
-    path = os.path.join(os.getcwd(), "tests//save_files") 
+    path = os.path.join(os.getcwd(), "tests//save_files")
     file_path = os.path.join(path, "test_save_ad")
-    
+
     ad = DAIndexGamma()
     loaded_ad = ad.load(file_path)
-    
+
     assert loaded_ad.dist_metric == "seuclidean"
     assert isinstance(loaded_ad.clf, RandomForestClassifier)
--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -15,9 +15,10 @@ SEED = 348
 np.random.seed(348)
 
 X = randint(5, size=(20, 5))
-y = np.array([0,0,1,1,0,1,0,1,0,1,0,0,1,1,0,1,0,1,0,1])
+y = np.array([0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=SEED)
 
 classifier = RandomForestClassifier(random_state=SEED)
 classifier.fit(X_train, y_train)
@@ -25,29 +26,33 @@ classifier.fit(X_train, y_train)
 ad = DAIndexGamma(clf=classifier)
 ad.fit(X_train)
 
+
 def test_sensitivity_specificity():
     y_pred, idx = ad.predict(X_test)
     sensitivity, specificity = sensitivity_specificity(y_test[idx], y_pred)
-    
+
     assert sensitivity >= 0 and sensitivity <= 1
     if str(specificity) == "nan":
         pass
     assert specificity >= 0 and specificity <= 1
-    
+
+
 def test_save_roc():
-    path = os.path.join(os.getcwd(), "tests//results") 
+    path = os.path.join(os.getcwd(), "tests//results")
     file_path = os.path.join(path, "test_result.png")
-    
+
     y_proba, idx = ad.predict_proba(X_test)
-    
+
     save_roc(y_test[idx], y_proba, file_path, title="Test ROC Curve")
-    
+
     assert os.path.exists(file_path)
-    
+
+
 def test_acc_vs_remove():
     y_pred, idx = ad.predict(X_test)
     acc_vs_removed(y_test, y_pred, y_pred)
-    
+
+
 def test_calculate_auc():
     y_pred, idx = ad.predict(X_test)
     y_true = y_test[idx]
