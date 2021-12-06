2c999f35e806384bf7e2a24883fed3dc9b0c92ca
bonjuruu <sarah.kyounghui@gmail.com>
2021-12-06 14:05:37 +1300

    Changed init for ProbabilityClassifier

--- adad/__init__.py
+++ adad/__init__.py
@@ -3,7 +3,7 @@ from .app_domain_base import AppDomainBase
 from .distance import (DISTANCE_METRICS, DAIndexDelta, DAIndexGamma,
                        DAIndexKappa)
 from .evaluate import save_roc, sensitivity_specificity
-from .probability import ProbabilityKNN, ProbabilityRFC, ProbabilitySVM
+from .probability import ProbabilityClassifier
 from .utils import (category2code, drop_redundant_col, get_range, maccs2binary,
                     set_seed, time2str)
 
@@ -11,7 +11,7 @@ __all__ = [
     'SklearnRegionBasedClassifier',
     'DISTANCE_METRICS', 'DAIndexDelta', 'DAIndexGamma', 'DAIndexKappa',
     'save_roc', 'sensitivity_specificity',
-    'ProbabilityRFC', 'ProbabilityKNN', 'ProbabilitySVM',
+    'ProbabilityClassifier',
     'category2code', 'drop_redundant_col', 'get_range', 'maccs2binary',
     'set_seed', 'time2str',
 ]
