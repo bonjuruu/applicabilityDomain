2f1c6d777c029ff863418c7ead766a3678ab6200
Luke Chang <luke.x.chang@gmail.com>
2022-01-09 18:18:39 +1300

    updated arguments in bash file

--- bash/run_exp.sh
+++ bash/run_exp.sh
@@ -4,7 +4,7 @@ DATALIST=("Ames" "BBBP" "Cancer" "CYP1A2" "hERG" "HIV" "Liver")
 CLASSIFIERS=("rf" "svm" "knn")
 CLF_ARGS=("{\"n_estimators\": 200}" "{\"C\": 10, \"probability\": true}" "{\"n_neighbors\": 5}")
 ADS=("gamma" "kappa" "delta" "boundingbox" "prob")
-AD_ARGS=("{\"dist_metric\": \"jaccard\"}" "{\"dist_metric\": \"jaccard\"}" "{\"dist_metric\": \"jaccard\"}" "{\"n_pc\": 5}" "{}")
+AD_ARGS=("{\"dist_metric\": \"jaccard\"}" "{\"dist_metric\": \"jaccard\"}" "{\"dist_metric\": \"jaccard\"}" "{\"n_components\": 5}" "{}")
 
 for DATA in ${DATALIST[@]}; do
     for ((I=0; I<${#CLASSIFIERS[@]}; I++)); do
