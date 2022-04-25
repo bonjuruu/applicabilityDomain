161cfb48c25ceaeb41374355ce97a653a1676381
Luke Chang <luke.x.chang@gmail.com>
2022-04-25 16:08:16 +1200

    fix bash script. added index

--- bash/advx/gen_advx.sh
+++ bash/advx/gen_advx.sh
@@ -7,8 +7,8 @@ for IDX in "${INDICES[@]}"; do
     for DATA in "${DATANAMES[@]}"; do
         echo "-------------------------------------------------------------"
         echo "[$IDX] Generating advx exmaples on $DATA..."
-        python ./experiments/advx/step3_attack.py -d $DATA -a fgsm -e 0.1 0.3 0.6 1.0 1.5
-        python ./experiments/advx/step3_attack.py -d $DATA -a apgd -e 0.1 0.3 0.6 1.0 1.5
-        python ./experiments/advx/step3_attack.py -d $DATA -a cw2 -e 0 5 10
+        python ./experiments/advx/step3_attack.py -d $DATA -i $IDX -a fgsm -e 0.1 0.3 0.6 1.0 1.5
+        python ./experiments/advx/step3_attack.py -d $DATA -i $IDX -a apgd -e 0.1 0.3 0.6 1.0 1.5
+        python ./experiments/advx/step3_attack.py -d $DATA -i $IDX -a cw2 -e 0 5 10
     done
 done
